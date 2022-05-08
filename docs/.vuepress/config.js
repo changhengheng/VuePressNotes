804c6a94ebbba44afc0e4dad0a8ca236dbd562cc
changhengheng <321121653@qq.com>
2022-05-08 18:08:51 +0800

    部署测试

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    title: 'my-blog', // 设置网站标题
+    dest: './dist',  // 设置输出目录
+    base: '/vuepressNotes/',// 设置站点根路径
+    repo:
+        'https://github.com/changhengheng/vuepressNotes.git' // 添加 github 链接
+}
